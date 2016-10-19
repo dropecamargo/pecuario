3eeccbafda522e7e7c0084dbdea75c28cb414ec2
Kevin <steverp95@outlook.es>
2016-10-19 10:56:49 -0500

    main

--- resources/assets/js/views/especies/main.js
+++ resources/assets/js/views/especies/main.js
@@ -3,24 +3,47 @@ app || (app = {});
 (function ($, window, document, undefined) {
 
     app.MainEspeciesView = Backbone.View.extend({
+
         el: '#especies-main',
+
         /**
         * Constructor Method
         */
         initialize : function() {
-            
+
             this.$especiesSearchTable = this.$('#especies-search-table');
 
             this.$especiesSearchTable.DataTable({
-				processing: true,
+                dom: "<'row'<'col-sm-4'B><'col-sm-4 text-center'l><'col-sm-4'f>>" +
+                    "<'row'<'col-sm-12'tr>>" +
+                    "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+                processing: true,
                 serverSide: true,
-            	language: window.Misc.dataTableES(),
+                language: window.Misc.dataTableES(),
                 ajax: window.Misc.urlFull( Route.route('especies.index') ),
                 columns: [
-                    { data: 'id', name: 'id' },
-                    { data: 'especie_nombre', name: 'especie_nombre'}
+                    { data: 'especie_nombre', name: 'especie_nombre' },
+                    { data: 'especie_activa', name: 'especie_activa' }
+                ],
+                buttons: [
+                    {
+                        text: '<i class="fa fa-user-plus"></i> Nueva especie',
+                        className: 'btn-sm',
+                        action: function ( e, dt, node, config ) {
+                            window.Misc.redirect( window.Misc.urlFull( Route.route('especies.create') ) )
+                        }
+                    }
+                ],
+                columnDefs: [
+                    {
+                        targets: 1,
+                        width: '10%',
+                        render: function ( data, type, full, row ) {
+                            return data ? 'Si' : 'No';
+                        }
+                    }
                 ]
-			});
+            });
         }
     });
 
